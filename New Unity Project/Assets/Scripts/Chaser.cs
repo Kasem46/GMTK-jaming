546f291ea88c8fd4f58917c3aa89b638c591fdcb
Dexo55 <Dexoh53@gmail.com>
2023-07-08 20:37:37 -0400

    Hexagon more like Deja Vu
    How odd, I feel as if I have made a similar commit before, almost as if the code stopped working and I had to rewrite it. I'm sure it's nothing.
    TL;DR: Hexagon is a real shape (maybe)

--- New Unity Project/Assets/Scripts/Chaser.cs
+++ New Unity Project/Assets/Scripts/Chaser.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chaser : MonoBehaviour{
+public class Chaser : MonoBehaviour
+{
+    public Vector3 mousePos4 = new Vector3();
+    public Vector3 point4 = new Vector3();
     public float moveSpeed = .5f;
     public Camera cam;
-    public GameObject cube;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update(){
-        
         if (Input.GetMouseButton(2)){
-            transform.position = Vector2.MoveTowards(transform.position, cube.GetComponent<Transform>().position, moveSpeed * Time.deltaTime);
+            mousePos4 = Input.mousePosition;
+            point4 = cam.ScreenToWorldPoint(new Vector3(mousePos4.x, mousePos4.y, +1));
+            if (CheckBound(point4) == true){
+                transform.position = Vector3.MoveTowards(transform.position, point4, moveSpeed * Time.deltaTime);
+            }
+            
         }
+    }
+    bool CheckBound(Vector3 mousePos) {
+        if (mousePos.x != Mathf.Clamp((float)mousePos.x, -2.75f, 2.75f) || mousePos.y != Mathf.Clamp((float)mousePos.y, -4.5f, 0f)) { return true; } else { return false; }
     }
 }
