e3167c9276841ea82bfd32d7cf751e0e8935b9a9
Dexo55 <Dexoh53@gmail.com>
2023-07-09 10:08:05 -0400

    Menu and NO exist now

---
+++ New Unity Project/Assets/Scripts/GoToScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+    public void moveToGame(){
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void moveToMenu(){
+        SceneManager.LoadScene("Menu");
+    }
+}
